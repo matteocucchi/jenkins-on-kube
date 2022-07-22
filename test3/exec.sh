34c7c408b18fa2b5ebf834137eae2e972813a44c
matteocucchi <matteo.cucchi98@gmail.com>
2022-07-22 09:13:24 +0200

    update

--- test3/exec.sh
+++ test3/exec.sh
@@ -1,5 +1,6 @@
 docker build -t jenkins:lts-docker .
 kubectl apply -f namespace.yaml
-kubectl apply -f volume.yaml
+kubectl apply -f serviceAccount.yaml
+kubectl create -f volume.yaml
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
