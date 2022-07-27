425cf1c70abedd2478063edfd8d40385e472d102
matteocucchi <matteo.cucchi98@gmail.com>
2022-07-27 17:15:06 +0200

    update

--- test3/oc-delete.sh
+++ test3/oc-delete.sh
@@ -1,5 +1,7 @@
-oc delete -f namespace.yaml
-oc delete -f serviceAccount.yaml
-oc delete -f volume.yaml
-oc delete -f deployment.yaml
 oc delete -f service.yaml
+oc delete -f deployment.yaml
+oc delete -f volume.yaml
+oc delete -f serviceAccount.yaml
+oc delete -f namespace.yaml
+
+
